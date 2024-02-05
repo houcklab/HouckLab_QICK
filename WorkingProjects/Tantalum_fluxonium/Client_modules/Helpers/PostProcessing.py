9061a72e22001af96d247d86802c51af95303851
Parth Jatakia <parth.jatakia@gmail.com>
2024-02-05 16:17:45 -0500

    Added SingleShotAnalysis
    This one is a major change. We are moving to a new way of calculating the errors for temperatures. Check SingleShotAnalysis.py to understand. The documentation will improve soon.

--- WorkingProjects/Tantalum_fluxonium/Client_modules/Helpers/PostProcessing.py
+++ WorkingProjects/Tantalum_fluxonium/Client_modules/Helpers/PostProcessing.py
@@ -1,38 +1,19 @@
-from WorkingProjects.Tantalum_fluxonium.Client_modules.Calib_escher.initialize import *
-from WorkingProjects.Tantalum_fluxonium.Client_modules.Experiments.mT1_PS_sse import T1_PS_sse
-from WorkingProjects.Tantalum_fluxonium.Client_modules.Experiments.mSingleShotTemp_sse import SingleShotSSE
-from WorkingProjects.Tantalum_fluxonium.Client_modules.Experiments.mQND import QNDmeas
-from WorkingProjects.Tantalum_fluxonium.Client_modules.Experiments.mTimeOfFlight_PS import TimeOfFlightPS
-from WorkingProjects.Tantalum_fluxonium.Client_modules.Experiments.mSpecSlice import SpecSlice
-
-import h5py
-import json
 import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import h5py
+from scipy.special import erfinv
+from tqdm import tqdm
+import json
 
-def h5_to_dict(filename):
-    dict = {}
-    with h5py.File(filename+".h5", 'r') as f:
-        for key in f.keys():
-            dict[key] = f[key][()]
-    return dict
+from lmfit import report_fit
 
-def json_to_dict(filename):
-    with open(filename+".json") as f:
-        data = f.read()
-    dict = json.loads(data)
-    return dict
+### define some constants
+hbar = sp.constants.hbar ###1.054e-34
+kb = sp.constants.k
+echarge = sp.constants.e
+phi0 = sp.constants.h / (2*echarge)
+
+from WorkingProjects.Tantalum_fluxonium.Client_modules.Helpers.SingleShotAnalysis import PS_Analysis
 
 
-filename = r"Z:\TantalumFluxonium\Data\2023_10_31_BF2_cooldown_6\WTF\TempChecks\Yoko_-0.37\T1_PS_temp_10\T1_PS_temp_10_2024_01_10\T1_PS_temp_10_2024_01_10_23_41_01_data"
-data = {"config": json_to_dict(filename) , "data": h5_to_dict(filename)}
-outerFolder = "Z:\\TantalumFluxonium\\Data\\2023_10_31_BF2_cooldown_6\\WTF\\TempChecks\\Yoko_" + str(data["config"]["yokoVoltage_freqPoint"]) + "\\"
-# Processing the data : T1
-Instance_T1 = T1_PS_sse(path="random", outerFolder="random",
-                        cfg=data["config"], soc=soc, soccfg=soccfg)
-Instance_T1.process_data(data)
-Instance_T1.display(data, plotDisp=True, saveFig = False)
-print(data["data"]["T1_guess"])
-# Processing the data : Single Shot Temperature
-# Instance_SingleShotProgram = SingleShotSSE(path="TempMeas_temp_" + str(data["config"]["fridge_temp"]),outerFolder=outerFolder, cfg=data["config"], soc=soc, soccfg=soccfg)
-# data_SingleShot = Instance_SingleShotProgram.process_data(data, bin_size=51)
-# Instance_SingleShotProgram.display(data_SingleShot, plotDisp=True, save_fig=True)
