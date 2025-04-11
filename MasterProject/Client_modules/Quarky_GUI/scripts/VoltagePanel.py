796c41944cd0d33e2ec5b94d25d162a22cbce066
Sonny Lowe <sonny@princeton.edu>
2025-04-11 11:18:56 -0400

    Voltage Interface implementation [not tested]

--- MasterProject/Client_modules/Quarky_GUI/scripts/VoltagePanel.py
+++ MasterProject/Client_modules/Quarky_GUI/scripts/VoltagePanel.py
@@ -420,4 +420,6 @@ class QVoltagePanel(QWidget):
                     formatted_json = json.dumps(self.config_tree_panel.config["Voltage Config"], indent=2)
         self.sweeps_edit.setText(formatted_json)
 
+        # TODO HANDLE EDIT UPDATING
+
 
