3efe4980a993d149ee320a17e8c7d1928e0fb1a1
Lev Krayzman <lev.v.krayzman@gmail.com>
2025-08-20 16:53:05 -0400

    fix small bug in mAmplitudeRabiBlob_PS, it still doesn't work

--- WorkingProjects/Tantalum_fluxonium_escher/Client_modules/Experiments/mAmplitudeRabiBlob_PS.py
+++ WorkingProjects/Tantalum_fluxonium_escher/Client_modules/Experiments/mAmplitudeRabiBlob_PS.py
@@ -63,7 +63,7 @@ class LoopbackProgramAmplitudeRabi_PS(RAveragerProgram):
             self.set_pulse_registers(ch=cfg["qubit_ch"], style=cfg["qubit_pulse_style"], freq=qubit_freq,
                                      phase=self.deg2reg(90, gen_ch=cfg["qubit_ch"]), gain=cfg["qubit_gain"],
                                      waveform="qubit", length=self.us2cycles(self.cfg["flat_top_length"], gen_ch = self.cfg["qubit_ch"]))
-            self.qubit_pulseLength = self.us2cycles(self.cfg["sigma"], gen_ch = self.cfg) * 4 + self.us2cycles(self.cfg["flat_top_length"])
+            self.qubit_pulseLength = self.us2cycles(self.cfg["sigma"], gen_ch = self.cfg["qubit_ch"]) * 4 + self.us2cycles(self.cfg["flat_top_length"])
 
         else:
             print("define pi or flat top pulse")
