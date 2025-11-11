c2ce53136692e9733581ba4e03ab3e78f31127f3
Sonny Lowe <sonny@princeton.edu>
2025-11-11 09:42:38 -0500

    Combining with old files

--- WorkingProjects/Triangle_Lattice_tProcV2/Flux_Files/Calculate_FF.py
+++ WorkingProjects/Triangle_Lattice_tProcV2/Flux_Files/Calculate_FF.py
@@ -1,83 +1,257 @@
+import os
+import json
+from typing import Any, Dict, List, Tuple, Optional
+
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+
+from WorkingProjects.Triangle_Lattice_tProcV2.Experiment import ExperimentClass
 
 from WorkingProjects.Triangle_Lattice_tProcV2.Helpers.Device_calibration import full_device_calib
 from Import_Functions_Transmon import *
 from Initialize_Qubit_Information import flux_sign, model_mapping
-
-
-
-
 from Whole_system_to_Voltages import flux_vector, dressed_qubit_freqs, coupler_freqs, beta_matrix
 
-plot_effective_system = True
-suffix = "BS"
-# (4100.0, 4419.1, 3600.0, 3900.0, 4250.0, 4250.0, 4250.0, 4250.0)
 
-# (4050.0, 3750.0, 4350.0, 3950.0, 3650.0, 4250.0, 4150.0, 3550.0)
-# (3600.0, 3600.0, 4000.0, 4400.0, 4400.0, 3700.0, 3700.0, 4300.0)
-# (3900.0, 3527.2, 4300.0, 4000.0, 3700.0, 4392.0, 4100.0, 3522.7)
-# 7584
-# (4000.0, 4000.0, 4000.0, 3700.0, 3700.0, 4000.0, 4300.0, 4300.0)
-# (3535.3, 4100.0, 4389.6, 3640.0, 3900.0, 4390.0, 3460.3, 4000.0)
+class CalculateFFExperiment(ExperimentClass):
+    """Wraps Calculate_FF script into an Experiment-style class.
 
-frequencies = {
-    'Q1': 3700,
-    'Q2': 3600,
-    'Q3': 4000,
-    'Q4': 4320,
-    'Q5': 3600,
-    'Q6': 3750,
-    'Q7': 3900,
-    'Q8': 4100,
-}
+    This class ignores soc / soccfg and only uses cfg, so it can be invoked uniformly
+    with the rest of your experiment suite (e.g., .acquire() and .display()).
+    """
+
+    # Default values,
+    config_template = {
+        "frequencies": {
+            'Q1': 0,
+            'Q2': 0,
+            'Q3': 0,
+            'Q4': 0,
+            'Q5': 0,
+            'Q6': 0,
+            'Q7': 0,
+            'Q8': 0,
+        }
+    }
+
+    def __init__(self, *args, **kwargs):
+        # No soc/soccfg required; just pass through to keep API consistent
+        self.outerFolder = ""
+        super().__init__(*args, **kwargs)
+        self.cfg = self.cfg or {}
+        self.cfg.setdefault('plot_effective_system', True)
+        self.cfg.setdefault('suffix', 'BS')
+
+    def acquire(self) -> Dict[str, Any]:
+        """Compute target fluxes, gains, and dressed frequencies; optionally plot.
+
+        Returns a data dict suitable for save/display.
+        """
+        cfg = self.cfg
+        plot_effective_system = bool(cfg.get('plot_effective_system', True))
+        suffix = str(cfg.get('suffix', 'BS'))
+        frequencies = cfg.get('frequencies', self.config_template["frequencies"])
+
+        try:
+            flux_was_given = {key: (freq < 10) for key, freq in frequencies.items()}
+            target_fluxes = np.copy(flux_vector)
+
+            mappings = ['Q1_bare', 'Q2_bare', 'Q3_bare', 'Q4_bare', 'Q5_bare', 'Q6_bare', 'Q7_bare', 'Q8_bare']
+            for index, key in enumerate(['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8']):
+                if frequencies.get(key) is not None:
+                    if flux_was_given[key]:  # flux given
+                        target_fluxes[index] = frequencies[key]
+
+                    elif not type(frequencies[key]) is str:  # dressed_freq -> bare_freq -> flux
+                        dressed_freq = frequencies[key]
+                        adjacent_couplers = np.nonzero(beta_matrix[index, 8:])[0] + 0
+                        bare_freq = full_device_calib.get_bare_freq(dressed_freq,
+                                                                    [coupler_freqs[c] for c in adjacent_couplers],
+                                                                    betas=[beta_matrix[index, c + 8] for c in
+                                                                           adjacent_couplers])
+                        flux = flux_sign[mappings[index]] * model_mapping[mappings[index]].flux(bare_freq / 1e3)
+
+                        target_fluxes[index] = flux
+
+            ##!!! models such as Q1_bare have the flux_quanta in FF gain (~100,000) stored in flux_quantum_voltage !!!
+            # TODO: find a better place to store this info... - Joshua 7/25/25
+            FF_flux_quanta = np.array([model_mapping[bare_mapping].flux_quantum_voltage for bare_mapping in mappings])
+
+            flux_changes = np.asarray(target_fluxes) - np.asarray(flux_vector)
+            flux_changes = flux_changes[:8]
+
+            gains_list = flux_changes * FF_flux_quanta
+
+            gains_list = np.rint(gains_list).astype(int)
+
+
+            bare_order_of_items = ['Q1_bare', 'Q2_bare', 'Q3_bare', 'Q4_bare', 'Q5_bare', 'Q6_bare', 'Q7_bare', 'Q8_bare',
+                                   'C1', 'C2', 'C3', 'C4', 'C5', 'C6']
+            bare_frequencies = [1000 * model_mapping[mapping].freq(flux) for mapping, flux in
+                                zip(bare_order_of_items, target_fluxes)]
+
+            dressed_freqs, g_matrix = full_device_calib.dress_system(bare_frequencies, beta_matrix=beta_matrix,
+                                                                     plot=False)
+        except Exception as e:
+            raise RuntimeError(f"Calculations failed: {e}")
+
+
+        # Printing Summary
+        np.set_printoptions(linewidth=100000)
+        print('[' + ', '.join(str(g) for g in gains_list) + ']')
+        print('# (' + ', '.join(f"{d:.1f}" for d in dressed_freqs) + ')')
+        for j, gain, freq in zip(range(1, 1 + len(gains_list)), gains_list, dressed_freqs):
+            print(f"FF_gain{j}_{suffix} = {gain:>6}  # {freq:.1f}")
+
+        # Return a tidy record for saving / plotting
+        data = {
+            'target_fluxes': np.asarray(target_fluxes, dtype=float),
+            'gains_list': np.asarray(gains_list, dtype=int),
+            'bare_order': bare_order_of_items,
+            'bare_frequencies': np.asarray(bare_frequencies, dtype=float),
+            'dressed_freqs': np.asarray(dressed_freqs, dtype=float),
+            'g_matrix': np.asarray(g_matrix),
+            'suffix': suffix,
+        }
+
+        # Plotting if specified
+        if plot_effective_system:
+            self.display(data)
+
+        return data
+
+    def display(self, data: Dict[str, Any], **kwargs) -> None:
+        """
+        `dress_system(..., plot=True)` already plotted, we simply call plt.show().
+        """
+        if not data:
+            data = self.data
+
+        print("Plotting FF")
+
+        dressed_freqs = data['dressed_freqs']
+        g_matrix = data['g_matrix']
+        full_device_calib.plot_dressed_system(dressed_freqs, g_matrix)
+        plt.show()
+        plt.pause(0.1)
+
+    def save_data(self, data: Dict[str, Any]) -> None:
+        pass
+
+
+def main():
+
+    # Frequencies
+    frequencies = {
+        'Q1': 4280,
+        'Q2': 4130,
+        'Q3': 4350,
+        'Q4': 3880,
+        'Q5': 3750,
+        'Q6': 3960,
+        'Q7': 4055,
+        'Q8': 4205,
+    }
+
+    cfg = {
+        'plot_effective_system': True,
+        'suffix': 'BS',
+        'frequencies': frequencies,
+    }
+
+    expt = CalculateFFExperiment(path='', prefix='CalculateFF', soc=None, soccfg=None, cfg=cfg)
+    expt.acquire() # Display determined by config field "plot_effective_system", can also call display manually
+
+if __name__ == '__main__':
+    main()
 
 
 
 
-flux_was_given = {key: (freq < 10) for key,freq in frequencies.items()}
+##############################################################################
+###################### Non Experiment Version (Old) ##########################
+##############################################################################
 
-target_fluxes = np.copy(flux_vector)
-
-
-mappings = ['Q1_bare', 'Q2_bare', 'Q3_bare', 'Q4_bare', 'Q5_bare', 'Q6_bare', 'Q7_bare', 'Q8_bare']
-for index, key in enumerate(['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8']):
-    if frequencies.get(key) is not None:
-        if flux_was_given[key]: # flux given
-            target_fluxes[index] = frequencies[key]
-
-        elif not type(frequencies[key]) is str : # dressed_freq -> bare_freq -> flux
-            dressed_freq = frequencies[key]
-            adjacent_couplers = np.nonzero(beta_matrix[index, 8:])[0] + 0
-            bare_freq = full_device_calib.get_bare_freq(dressed_freq, [coupler_freqs[c] for c in adjacent_couplers], betas= [beta_matrix[index, c+8] for c in adjacent_couplers])
-            flux = flux_sign[mappings[index]]*model_mapping[mappings[index]].flux(bare_freq / 1e3)
-
-            target_fluxes[index] = flux
-
-##!!! models such as Q1_bare have the flux_quanta in FF gain (~100,000) stored in flux_quantum_voltage !!!
-# TODO: find a better place to store this info... - Joshua 7/25/25
-FF_flux_quanta = np.array([model_mapping[bare_mapping].flux_quantum_voltage for bare_mapping in mappings])
-
-flux_changes = np.asarray(target_fluxes) - np.asarray(flux_vector)
-flux_changes = flux_changes[:8]
-
-gains_list = flux_changes * FF_flux_quanta
-
-gains_list = np.rint(gains_list).astype(int)
-
-np.set_printoptions(linewidth=100000)
-print('['+', '.join(str(g) for g in gains_list)+']')
-
-
-bare_order_of_items = ['Q1_bare', 'Q2_bare', 'Q3_bare', 'Q4_bare', 'Q5_bare', 'Q6_bare', 'Q7_bare', 'Q8_bare',
-                   'C1', 'C2', 'C3', 'C4', 'C5', 'C6']
-bare_frequencies = [1000*model_mapping[mapping].freq(flux) for mapping,flux in zip(bare_order_of_items, target_fluxes)]
-
-dressed_freqs, g_matrix = full_device_calib.dress_system(bare_frequencies, beta_matrix=beta_matrix, plot=plot_effective_system)
-print('# ('+', '.join(f'{d:.1f}' for d in dressed_freqs)+')')
-for j, gain, freq in zip(range(1, 1+len(gains_list)), gains_list, dressed_freqs):
-    print(f"FF_gain{j}_{suffix} = {gain:>6}  # {freq:.1f}")
-
-
-
-plt.show()
+# import numpy as np
+# from matplotlib import pyplot as plt
+#
+# from WorkingProjects.Triangle_Lattice_tProcV2.Helpers.Device_calibration import full_device_calib
+# from Import_Functions_Transmon import *
+# from Initialize_Qubit_Information import flux_sign, model_mapping
+#
+#
+#
+#
+# from Whole_system_to_Voltages import flux_vector, dressed_qubit_freqs, coupler_freqs, beta_matrix
+#
+# plot_effective_system = True
+# suffix = "BS"
+# # (4100.0, 4419.1, 3600.0, 3900.0, 4250.0, 4250.0, 4250.0, 4250.0)
+#
+# # (4050.0, 3750.0, 4350.0, 3950.0, 3650.0, 4250.0, 4150.0, 3550.0)
+# # (3600.0, 3600.0, 4000.0, 4400.0, 4400.0, 3700.0, 3700.0, 4300.0)
+# # (3900.0, 3527.2, 4300.0, 4000.0, 3700.0, 4392.0, 4100.0, 3522.7)
+# # 7584
+# # (4000.0, 4000.0, 4000.0, 3700.0, 3700.0, 4000.0, 4300.0, 4300.0)
+# # (3535.3, 4100.0, 4389.6, 3640.0, 3900.0, 4390.0, 3460.3, 4000.0)
+#
+# frequencies = {
+#     'Q1': 3700,
+#     'Q2': 3600,
+#     'Q3': 4000,
+#     'Q4': 4320,
+#     'Q5': 3600,
+#     'Q6': 3750,
+#     'Q7': 3900,
+#     'Q8': 4100,
+# }
+#
+#
+#
+#
+# flux_was_given = {key: (freq < 10) for key,freq in frequencies.items()}
+#
+# target_fluxes = np.copy(flux_vector)
+#
+#
+# mappings = ['Q1_bare', 'Q2_bare', 'Q3_bare', 'Q4_bare', 'Q5_bare', 'Q6_bare', 'Q7_bare', 'Q8_bare']
+# for index, key in enumerate(['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8']):
+#     if frequencies.get(key) is not None:
+#         if flux_was_given[key]: # flux given
+#             target_fluxes[index] = frequencies[key]
+#
+#         elif not type(frequencies[key]) is str : # dressed_freq -> bare_freq -> flux
+#             dressed_freq = frequencies[key]
+#             adjacent_couplers = np.nonzero(beta_matrix[index, 8:])[0] + 0
+#             bare_freq = full_device_calib.get_bare_freq(dressed_freq, [coupler_freqs[c] for c in adjacent_couplers], betas= [beta_matrix[index, c+8] for c in adjacent_couplers])
+#             flux = flux_sign[mappings[index]]*model_mapping[mappings[index]].flux(bare_freq / 1e3)
+#
+#             target_fluxes[index] = flux
+#
+# ##!!! models such as Q1_bare have the flux_quanta in FF gain (~100,000) stored in flux_quantum_voltage !!!
+# # TODO: find a better place to store this info... - Joshua 7/25/25
+# FF_flux_quanta = np.array([model_mapping[bare_mapping].flux_quantum_voltage for bare_mapping in mappings])
+#
+# flux_changes = np.asarray(target_fluxes) - np.asarray(flux_vector)
+# flux_changes = flux_changes[:8]
+#
+# gains_list = flux_changes * FF_flux_quanta
+#
+# gains_list = np.rint(gains_list).astype(int)
+#
+# np.set_printoptions(linewidth=100000)
+# print('['+', '.join(str(g) for g in gains_list)+']')
+#
+#
+# bare_order_of_items = ['Q1_bare', 'Q2_bare', 'Q3_bare', 'Q4_bare', 'Q5_bare', 'Q6_bare', 'Q7_bare', 'Q8_bare',
+#                    'C1', 'C2', 'C3', 'C4', 'C5', 'C6']
+# bare_frequencies = [1000*model_mapping[mapping].freq(flux) for mapping,flux in zip(bare_order_of_items, target_fluxes)]
+#
+# dressed_freqs, g_matrix = full_device_calib.dress_system(bare_frequencies, beta_matrix=beta_matrix, plot=plot_effective_system)
+# print('# ('+', '.join(f'{d:.1f}' for d in dressed_freqs)+')')
+# for j, gain, freq in zip(range(1, 1+len(gains_list)), gains_list, dressed_freqs):
+#     print(f"FF_gain{j}_{suffix} = {gain:>6}  # {freq:.1f}")
+#
+#
+#
+# plt.show()
--- WorkingProjects/Triangle_Lattice_tProcV2/Flux_Files/plot_frequencies.py
+++ WorkingProjects/Triangle_Lattice_tProcV2/Flux_Files/plot_frequencies.py
@@ -1,54 +1,262 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from typing import Any, Dict, List
 
+from WorkingProjects.Triangle_Lattice_tProcV2.Experiment import ExperimentClass
 
-# import qubit parameters from this file
 from WorkingProjects.Triangle_Lattice_tProcV2.Run_Experiments.qubit_parameter_files.Qubit_Parameters_Master import *
-
 from Initialize_Qubit_Information import model_mapping
 from Whole_system_to_Voltages import flux_vector, beta_matrix
 from WorkingProjects.Triangle_Lattice_tProcV2.Helpers.Device_calibration import full_device_calib
 
-def ff_gains_to_freqs(ff_gains):
-    mappings = ['Q1_bare', 'Q2_bare', 'Q3_bare', 'Q4_bare', 'Q5_bare', 'Q6_bare', 'Q7_bare', 'Q8_bare']
-    FF_flux_quanta = np.array([model_mapping[bare_mapping].flux_quantum_voltage for bare_mapping in mappings])
 
-    flux_changes = np.asarray(ff_gains) / FF_flux_quanta
+class PlotFrequenciesExperiment(ExperimentClass):
+    """
+    Run the original 'FF gains → dressed frequencies across sections' calculation
+    as a drop-in experiment (no soc/soccfg needed).
+    """
 
-    target_fluxes = flux_vector + np.concatenate([flux_changes, np.zeros(6)])
+    # Default values,
+    config_template = {
+        "gains": []
+    }
 
-    bare_order_of_items = ['Q1_bare', 'Q2_bare', 'Q3_bare', 'Q4_bare', 'Q5_bare', 'Q6_bare', 'Q7_bare', 'Q8_bare',
-                       'C1', 'C2', 'C3', 'C4', 'C5', 'C6']
-    bare_frequencies = [1000*model_mapping[mapping].freq(flux) for mapping,flux in zip(bare_order_of_items, target_fluxes)]
+    # Hardcoded
+    coupled_pairs = [
+        # top rail
+        (1, 3), (3, 5), (5, 7),
+        # bottom rail
+        (2, 4), (4, 6), (6, 8),
+        # diagonals (up-right)
+        (2, 3), (4, 5), (6, 7),
+        # diagonals (down-right)
+        (1, 4), (3, 6), (5, 8),
+        # Added for testing intersection
+        (4,7),
+    ]
 
-    dressed_freqs, g_matrix = full_device_calib.dress_system(bare_frequencies, beta_matrix=beta_matrix, plot=False)
+    def __init__(self, *args, **kwargs):
+        # No soc/soccfg required; just pass through to keep API consistent
+        self.outerFolder = ""
+        super().__init__(*args, **kwargs)
+        self.cfg = self.cfg or {}
+        self.cfg.setdefault('plot', True)  # Plot by default
+        self.data = None
 
-    return dressed_freqs
+    @staticmethod
+    def ff_gains_to_freqs(ff_gains):
+        mappings = ['Q1_bare', 'Q2_bare', 'Q3_bare', 'Q4_bare', 'Q5_bare', 'Q6_bare', 'Q7_bare', 'Q8_bare']
+        FF_flux_quanta = np.array([model_mapping[bare_mapping].flux_quantum_voltage for bare_mapping in mappings])
 
-readout_gains = readout_params['1']['Readout']['FF_Gains']
+        flux_changes = np.asarray(ff_gains) / FF_flux_quanta
 
-intermediate_jump_gains = [-13804, None, -497, None, -17936, None, -2498, None]
-for i in range(len(intermediate_jump_gains)):
-    if intermediate_jump_gains[i] is None:
-        intermediate_jump_gains[i] = BS_FF[i]
+        target_fluxes = flux_vector + np.concatenate([flux_changes, np.zeros(6)])
 
-# gains = [Pulse_4815_FF, Init_FF, Ramp_FF, BS_FF, Readout_1234_FF]
-gains = [readout_gains, Init_FF, Ramp_FF, BS_FF, readout_gains]
-gains = [Readout_1234_FF, Init_FF, Ramp_FF, BS_FF, readout_gains]
-gains = [Readout_1234_FF, Init_FF, Ramp_FF, intermediate_jump_gains, BS_FF, readout_gains]
+        bare_order_of_items = ['Q1_bare', 'Q2_bare', 'Q3_bare', 'Q4_bare', 'Q5_bare', 'Q6_bare', 'Q7_bare', 'Q8_bare',
+                               'C1', 'C2', 'C3', 'C4', 'C5', 'C6']
+        bare_frequencies = [1000 * model_mapping[mapping].freq(flux) for mapping, flux in
+                            zip(bare_order_of_items, target_fluxes)]
+
+        dressed_freqs, g_matrix = full_device_calib.dress_system(bare_frequencies, beta_matrix=beta_matrix, plot=False)
+
+        return dressed_freqs
+
+    def acquire(self) -> Dict[str, Any]:
+        """
+        Not much here most done in main section below.
+        """
+        cfg = self.cfg
+        gains = cfg["gains"]
+
+        freqs = np.array([PlotFrequenciesExperiment.ff_gains_to_freqs(arr) for arr in tuple(gains)])
 
 
-freqs = np.array([ff_gains_to_freqs(arr) for arr in tuple(gains)])
+        # Printing out frequencies
+        for i in range(freqs.shape[0]):
+            print(list(int(x) for x in freqs[i, :]))
 
-for i in range(len(gains)):
-    print(list([int(x) for x in freqs[i,:]]))
+        data = {
+            'gains': gains,
+            'freqs': freqs,
+        }
+        self.data = data
 
-# gains = np.array([pulse_145, Readout_FF4])
-for i in range(freqs.shape[1]):
-    plt.plot(freqs[:,i], '-o', label=f'Q{i+1}')
-plt.xlabel('experimental section index')
-plt.ylabel('Frequency (MHz)')
-plt.legend()
-plt.show()
+        if cfg.get('plot', True):
+            self.display(data)
+
+        return data
+
+    def display(self, data: Dict[str, Any] = None, **kwargs) -> None:
+        """
+        Plot frequency trajectory per qubit across sections.
+        """
+        if data is None:
+            data = self.data
+        if not data:
+            return
+
+        freqs = data['freqs']
+        num_sections, num_qubits = freqs.shape
+
+        for qi in range(num_qubits):
+            plt.plot(freqs[:, qi], '-o', label=f'Q{qi+1}')
+        plt.xlabel('experimental section index')
+        plt.ylabel('Frequency (MHz)')
+        plt.legend()
+        plt.show()
+        plt.pause(0.1)
+
+    def _segment_intersection(self, p1, p2, p3, p4):
+        x1, y1 = p1;
+        x2, y2 = p2
+        x3, y3 = p3;
+        x4, y4 = p4
+        denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+        if abs(denom) < 1e-12:
+            return None
+        t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / denom
+        u = ((x1 - x3) * (y1 - y2) - (y1 - y3) * (x1 - x2)) / denom
+        if 0.0 <= t <= 1.0 and 0.0 <= u <= 1.0:
+            xi = x1 + t * (x2 - x1)
+            yi = y1 + t * (y2 - y1)
+            return (xi, yi)
+        return None
+
+    def display(self, data: Dict[str, Any] = None, **kwargs) -> None:
+        """
+        Plot frequency trajectory per qubit across sections.
+        """
+        if data is None:
+            data = self.data
+        if not data:
+            return
+
+        freqs = data['freqs']
+        S, Q = freqs.shape
+        xs = np.arange(S, dtype=float)
+
+        coupled_pairs = self.coupled_pairs
+        label_offset = 15.0
+
+        # plot and label
+        for qi in range(Q):
+            plt.plot(xs, freqs[:, qi], '-o', label=f'Q{qi + 1}')
+            for s in [0, S-1]: # Only label first and last or else they overlap
+                plt.annotate(f'Q{qi + 1}', (xs[s], freqs[s, qi] + label_offset), fontsize=8,
+                             ha='center', va='bottom')
+
+        # crossings on last ramp
+        k_end = S - 1
+        k0 = max(0, k_end - 1)
+        for (a1, a2) in coupled_pairs:
+            i = a1 - 1;
+            j = a2 - 1
+            if i < 0 or i >= Q or j < 0 or j >= Q:
+                continue
+            p1 = (xs[k0], freqs[k0, i]);
+            p2 = (xs[k_end], freqs[k_end, i])
+            p3 = (xs[k0], freqs[k0, j]);
+            p4 = (xs[k_end], freqs[k_end, j])
+            pt = self._segment_intersection(p1, p2, p3, p4)
+            if pt is not None:
+                plt.scatter([pt[0]], [pt[1]], marker='^', s=100, color='red', zorder=5)
+                plt.text(pt[0]+0.009, pt[1]-5, '!', color='white', fontsize=10,
+                         ha='center', va='center', fontweight='bold', zorder=6)
+
+        plt.xticks(xs)
+        plt.xlabel('experimental section index')
+        plt.ylabel('Frequency (MHz)')
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+        plt.pause(0.1)
+
+    def save_data(self, data: Dict[str, Any]) -> None:
+        pass
+
+
+def main():
+
+    # Setting gains
+    intermediate_jump_gains = [-13449, None, 8706, None, -5250, None, 19650, None]
+    for i in range(len(intermediate_jump_gains)):
+        if intermediate_jump_gains[i] is None:
+            intermediate_jump_gains[i] = BS_FF[i]
+
+    readout_gains = readout_params['1']['Readout']['FF_Gains']
+
+    # gains = [Pulse_4815_FF, Init_FF, Ramp_FF, BS_FF, Readout_1234_FF]
+    gains = [readout_gains, Init_FF, Ramp_FF, BS_FF, readout_gains]
+    gains = [Readout_1234_FF, Init_FF, Ramp_FF, BS_FF, readout_gains]
+    gains = [Readout_1234_FF, Init_FF, Ramp_FF, intermediate_jump_gains, BS_FF, readout_gains]
+
+
+    config = {'gains': gains, 'plot': True}
+    expt = PlotFrequenciesExperiment(path='', prefix='PlotFrequencies', soc=None, soccfg=None, cfg=config)
+    expt.acquire() # Also performs plotting if "plot" True
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+##############################################################################
+###################### Non Experiment Version (Old) ##########################
+##############################################################################
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+#
+# # import qubit parameters from this file
+# from WorkingProjects.Triangle_Lattice_tProcV2.Run_Experiments.qubit_parameter_files.Qubit_Parameters_Master import *
+#
+# from Initialize_Qubit_Information import model_mapping
+# from Whole_system_to_Voltages import flux_vector, beta_matrix
+# from WorkingProjects.Triangle_Lattice_tProcV2.Helpers.Device_calibration import full_device_calib
+#
+# def ff_gains_to_freqs(ff_gains):
+#     mappings = ['Q1_bare', 'Q2_bare', 'Q3_bare', 'Q4_bare', 'Q5_bare', 'Q6_bare', 'Q7_bare', 'Q8_bare']
+#     FF_flux_quanta = np.array([model_mapping[bare_mapping].flux_quantum_voltage for bare_mapping in mappings])
+#
+#     flux_changes = np.asarray(ff_gains) / FF_flux_quanta
+#
+#     target_fluxes = flux_vector + np.concatenate([flux_changes, np.zeros(6)])
+#
+#     bare_order_of_items = ['Q1_bare', 'Q2_bare', 'Q3_bare', 'Q4_bare', 'Q5_bare', 'Q6_bare', 'Q7_bare', 'Q8_bare',
+#                        'C1', 'C2', 'C3', 'C4', 'C5', 'C6']
+#     bare_frequencies = [1000*model_mapping[mapping].freq(flux) for mapping,flux in zip(bare_order_of_items, target_fluxes)]
+#
+#     dressed_freqs, g_matrix = full_device_calib.dress_system(bare_frequencies, beta_matrix=beta_matrix, plot=False)
+#
+#     return dressed_freqs
+#
+# readout_gains = readout_params['1']['Readout']['FF_Gains']
+#
+# intermediate_jump_gains = [-13804, None, -497, None, -17936, None, -2498, None]
+# for i in range(len(intermediate_jump_gains)):
+#     if intermediate_jump_gains[i] is None:
+#         intermediate_jump_gains[i] = BS_FF[i]
+#
+# # gains = [Pulse_4815_FF, Init_FF, Ramp_FF, BS_FF, Readout_1234_FF]
+# gains = [readout_gains, Init_FF, Ramp_FF, BS_FF, readout_gains]
+# gains = [Readout_1234_FF, Init_FF, Ramp_FF, BS_FF, readout_gains]
+# gains = [Readout_1234_FF, Init_FF, Ramp_FF, intermediate_jump_gains, BS_FF, readout_gains]
+#
+#
+# freqs = np.array([ff_gains_to_freqs(arr) for arr in tuple(gains)])
+#
+# for i in range(len(gains)):
+#     print(list([int(x) for x in freqs[i,:]]))
+#
+# # gains = np.array([pulse_145, Readout_FF4])
+# for i in range(freqs.shape[1]):
+#     plt.plot(freqs[:,i], '-o', label=f'Q{i+1}')
+# plt.xlabel('experimental section index')
+# plt.ylabel('Frequency (MHz)')
+# plt.legend()
+# plt.show()
 
 
