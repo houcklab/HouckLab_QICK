fc977b31c492cf4168dbcb4c4292b6f307ef02ec
De Leon Lab <deleonlab@princeton.edu>
2026-04-22 16:20:09 -0400

    Loopback tests for RFBoard

--- WorkingProjects/gates_team/Experiments/Tests/mConstantToneTest.py
+++ WorkingProjects/gates_team/Experiments/Tests/mConstantToneTest.py
@@ -4,6 +4,7 @@ from qick.asm_v2 import AveragerProgramV2
 
 from WorkingProjects.gates_team.CoreLib.socProxy import makeProxy
 
+
 class PulseTest(AveragerProgramV2):
     """ """
 
@@ -28,19 +29,19 @@ class PulseTest(AveragerProgramV2):
     def _body(self, cfg):
         self.pulse(cfg["gen_ch"], name="mypulse")
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     config = {
         "gen_ch": 8,
         "nqz": 1,
         "mixer_freq": 800,
         "reps": 1,
         "pulse_length": 60,
-        "freq": 1500, # difference between freq and mixer_freq must be within mixer range/2
+        "freq": 1500,  # difference between freq and mixer_freq must be within mixer range/2
         "period": 2,
         "phrst": 1,
         "phase": 0,
-        "gain": 0, # from -1 to +1
+        "gain": 0,  # from -1 to +1
     }
 
     soc, soccfg = makeProxy()
@@ -48,13 +49,13 @@ if __name__ == "__main__":
     prog.config_all(soc)
 
     # Set Nyquist Zone.
-    dacid = soccfg['gens'][config['gen_ch']]['dac']
+    dacid = soccfg["gens"][config["gen_ch"]]["dac"]
     soc.rf.set_nyquist(dacid, nqz=config["nqz"])
 
     # Set Filter.
-    soc.rfb_set_gen_filter(gen_ch=config['gen_ch'], fc=3, ftype='lowpass')
+    soc.rfb_set_gen_filter(gen_ch=config["gen_ch"], fc=3, ftype="lowpass")
 
     # Set attenuator on DAC.
-    soc.rfb_set_gen_rf(gen_ch=config['gen_ch'], att1=0, att2=10)
+    soc.rfb_set_gen_rf(gen_ch=config["gen_ch"], att1=0, att2=10)
 
     soc.tproc.start()
